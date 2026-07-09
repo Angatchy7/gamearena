23bf6a8d87fb4a9b48dd871985a573871f1b86bd
Angat Chaudhary <angat3950@gmail.com>
2026-07-09 10:52:45 +0545

    feat: implement team model and admin integration

--- apps/teams/admin.py
+++ apps/teams/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "created_by",
+        "is_active",
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,3 +1,55 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        blank=True,
+    )
+
+    description = models.TextField(
+        blank=True,
+    )
+
+    logo = models.ImageField(
+        upload_to="teams/logos/",
+        blank=True,
+        null=True,
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    created_by = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.PROTECT,
+    related_name="created_teams",
+)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
